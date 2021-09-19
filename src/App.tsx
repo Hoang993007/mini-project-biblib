10f7a0bf13c9a80440f76399651583072bd597d5
MinhHoang993007 <nguyenminhhoang1999.tg@gmail.com>
2021-09-19 14:24:51 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'src/styles/_base_style.scss';
+import 'src/styles/_base.scss';
+
+const App: React.FC<any> = () => {
+  return <div className="App">Hello</div>;
+};
+
+export default App;
